9fd92fd77f41d5b2200d55eaad2b1529dd884736
Emil Filip <emilfilip@hotmail.com>
2023-09-13 19:07:09 +0100

    Updated projects to .Net 7; Updated nuget packages; Updated yml files

---
+++ EFSoft.Inventory.WebJob/Functions.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.Azure.WebJobs;
+
+namespace EFSoft.Inventory.WebJob
+{
+    public class Functions
+    {
+        public async Task ProcessInventoryOrder([ServiceBusTrigger("orderplaced", "InventoryOrderPlaced", Connection = "TopicConnectionString")] string myTopicMessage)
+        {
+            //_logger.LogInformation($"C# ServiceBus topic trigger function processed message: {myTopicMessage}");
+
+            //var orderPlacedMessage = JsonSerializer.Deserialize<OrderPlaced>(myTopicMessage);
+
+            //var parameters = new DecreaseInventoryStockCommandParameters(
+            //    productId: orderPlacedMessage.ProductId,
+            //    stockToSubtract: orderPlacedMessage.Quantity);
+
+            //await _commandExecutor.ExecuteAsync(parameters);
+        }
+    }
+}
---
+++ EFSoft.Inventory.WebJob/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new HostBuilder();
+
+        //builder.ConfigureWebJobs(bldr =>
+        //{
+        //    bldr.AddServiceBus(sb =>
+        //    { 
+        //        sb.ClientRetryOptions = 
+        //    });
+        //});
+
+        //builder.ConfigureLogging(logCfg =>
+        //{
+        //    logCfg.AddConsole();
+        //});
+
+        var host = builder.Build();
+
+        using (host)
+        {
+            host.Run();
+        }
+    }
+}
