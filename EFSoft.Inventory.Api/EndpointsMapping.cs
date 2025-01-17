239341d017fcccb1dc02cde2b38ed70b63019e63
EmilFilip <emilfilip@hotmail.com>
2025-01-17 12:56:23 +0000

    Fixed route

--- EFSoft.Inventory.Api/EndpointsMapping.cs
+++ EFSoft.Inventory.Api/EndpointsMapping.cs
@@ -4,7 +4,7 @@ public class EndpointsMapping : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        var group = app.MapGroup("api/product").WithTags("Inventory");
+        var group = app.MapGroup("api/inventory").WithTags("Inventory");
 
         _ = group.MapGet("/{productId:guid}", GetInventoryEndpoint.GetInventory);
 
